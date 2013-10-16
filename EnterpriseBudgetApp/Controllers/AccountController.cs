c4018859e9e44b3db66da6b1371c39f48b805c8d
DiskImage <ian@iangrav.es>
2013-10-16 12:36:42 -0400

    Added fields to Register() in the AccountController

--- EnterpriseBudgetApp/Controllers/AccountController.cs
+++ EnterpriseBudgetApp/Controllers/AccountController.cs
@@ -79,7 +79,7 @@ namespace EnterpriseBudgetApp.Controllers
                 // Attempt to register the user
                 try
                 {
-                    WebSecurity.CreateUserAndAccount(model.UserName, model.Password);
+                    WebSecurity.CreateUserAndAccount(model.UserName, model.Password, new {UserEmail = model.UserEmail, Name = model.Name});
                     WebSecurity.Login(model.UserName, model.Password);
                     return RedirectToAction("Index", "Home");
                 }
