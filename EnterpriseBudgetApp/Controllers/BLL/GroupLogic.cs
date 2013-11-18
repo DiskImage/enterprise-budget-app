cbce194ab6369883ba6593872951e338f5ca094a
DiskImage <ian@iangrav.es>
2013-11-17 23:00:57 -0500

    added basic groups CRUD to site. Links creating user to new group as a super admin, manually cascades the delete from groups to the join table entries.

---
+++ EnterpriseBudgetApp/Controllers/BLL/GroupLogic.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using EnterpriseBudgetApp.Models;
+
+namespace EnterpriseBudgetApp.Controllers.BLL
+{
+    public class GroupLogic
+    {
+        private vm343_01aEntities db = null;
+
+        public GroupLogic()
+        {
+            this.db = new vm343_01aEntities();
+        }
+
+        public bool createGroup(Group group)
+        {
+            bool success = false;
+            int currentUserId = (int)Membership.GetUser().ProviderUserKey;
+
+            db.Groups.Add(group);
+            this.addGroupMember(group, currentUserId, 0); //0 is super-admin roleId.
+            db.SaveChanges();
+
+            return success;
+        }
+
+        private void addGroupMember(Group group, int userId, int roleId)
+        {
+            Group_Users gu = new Group_Users();
+            gu.GroupId = group.GroupId;
+            gu.UserId = userId;
+            gu.RoleId = roleId;
+            gu.deleted = false;
+            db.Group_Users.Add(gu);
+        }
+    }
+}
---
+++ EnterpriseBudgetApp/Controllers/GroupController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EnterpriseBudgetApp.Controllers.BLL;
+using EnterpriseBudgetApp.Models;
+
+namespace EnterpriseBudgetApp.Controllers
+{
+    public class GroupController : Controller
+    {
+        private vm343_01aEntities db = new vm343_01aEntities();
+        private GroupLogic grpLogic = new GroupLogic();
+
+        //
+        // GET: /Group/
+
+        public ActionResult Index()
+        {
+            return View(db.Groups.ToList());
+        }
+
+        //
+        // GET: /Group/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Group group = db.Groups.Find(id);
+            if (group == null)
+            {
+                return HttpNotFound();
+            }
+            return View(group);
+        }
+
+        //
+        // GET: /Group/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Group/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Group group)
+        {
+            if (ModelState.IsValid)
+            {
+                grpLogic.createGroup(group);
+//                db.Groups.Add(group);
+//                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(group);
+        }
+
+        //
+        // GET: /Group/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Group group = db.Groups.Find(id);
+            if (group == null)
+            {
+                return HttpNotFound();
+            }
+            return View(group);
+        }
+
+        //
+        // POST: /Group/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Group group)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(group).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(group);
+        }
+
+        //
+        // GET: /Group/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Group group = db.Groups.Find(id);
+            if (group == null)
+            {
+                return HttpNotFound();
+            }
+            return View(group);
+        }
+
+        //
+        // POST: /Group/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            //TO-MOVE: BLL
+            Group group = db.Groups.Find(id);
+            db.Groups.Remove(group);
+            var query = from p in db.Group_Users
+                            where p.GroupId == id
+                            select p;
+            IEnumerable<Group_Users> grp_usrs = query.ToList();
+            foreach (Group_Users to_kill in grp_usrs)
+            {
+                db.Group_Users.Remove(to_kill);
+            }
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
