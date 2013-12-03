c027b551c3c1cb00d757156fba8ec8cbbd82a11d
rxk8345 <rxk8345@g.rit.edu>
2013-12-03 14:12:13 -0500

    Example Pie Chart is working

--- EnterpriseBudgetApp/Controllers/ReportController.cs
+++ EnterpriseBudgetApp/Controllers/ReportController.cs
@@ -10,6 +10,9 @@ using EnterpriseBudgetApp.Models;
 using System.Web.Security;
 using DotNet.Highcharts.Options;
 using EnterpriseBudgetApp.Filters;
+using DotNet.Highcharts.Enums;
+using System.Drawing;
+using DotNet.Highcharts.Helpers;
 
 namespace EnterpriseBudgetApp.Controllers
 {
@@ -23,27 +26,66 @@ namespace EnterpriseBudgetApp.Controllers
         // GET: /Report/
         public ActionResult Index()
         {
-            int currentUserId = (int) Membership.GetUser().ProviderUserKey;
+            int currentUserId = (int)Membership.GetUser().ProviderUserKey;
             var transactions = db.Transactions.Include(t => t.TransType1).Include(t => t.UserProfile);
             Transaction[] trans = transactions.Where(t => t.AcctId.Equals(currentUserId)).ToArray();
-            String[] cat = null ;
-            Object[] data = null;
+            List<Object> dataSeries = new List<Object>();
+            List<String> cat = new List<String>();
+            List<Object> data = new List<Object>();
 
-            for(int i = 0; i < trans.Length ; i++)
+            for (int i = 0; i < trans.Length; i++)
             {
-                 cat[i] = trans[i].TransType.ToString();
-                 data[i] = trans[i].Amount;
+                data.Add(trans[i].Amount);
+                cat.Add(trans[i].TransType1.Name);
+                dataSeries.Add(new Object[] { trans[i].Amount, trans[i].TransType1.Name });
             }
 
 
             DotNet.Highcharts.Highcharts chart = new DotNet.Highcharts.Highcharts("chart")
-.SetXAxis(new XAxis
-{
-    Categories = cat
-})
+            .SetPlotOptions(new PlotOptions
+            {
+                Pie = new PlotOptionsPie
+                {
+
+                    AllowPointSelect = true,
+                    Cursor = Cursors.Pointer,
+                    DataLabels = new PlotOptionsPieDataLabels
+        {
+            Color = ColorTranslator.FromHtml("#000000"),
+            ConnectorColor = ColorTranslator.FromHtml("#000000"),
+            Formatter = "function() { return '<b>'+ this.point.name +'</b>: '+ this.percentage +' %'; }"
+        },
+                    Point = new PlotOptionsPiePoint
+                    {
+                        Events = new PlotOptionsPiePointEvents
+                        {
+                            Click = "function() { alert (this.category +': '+ this.y); }"
+
+                        }
+                    }
+
+                }
+            })
 .SetSeries(new Series
 {
-    Data = new DotNet.Highcharts.Helpers.Data( data )
+    Type = ChartTypes.Pie,
+    Name = "Browser share",
+    Data = new Data( new object[]
+                                           {
+                                               new object[] { "Firefox", 45.0 },
+                                               new object[] { "IE", 26.8 },
+                                               new DotNet.Highcharts.Options.Point
+                                               {
+                                                   Name = "Chrome",
+                                                   Y = 12.8,
+                                                   Sliced = true,
+                                                   Selected = true
+                                               },
+                                               new object[] { "Safari", 8.5 },
+                                               new object[] { "Opera", 6.2 },
+                                               new object[] { "Others", 0.7 }
+                                           })
+
 });
 
             return View(chart);
@@ -51,4 +93,7 @@ namespace EnterpriseBudgetApp.Controllers
         }
 
     }
+
+
+
 }
